de5bb2a4611758208013620e0d2e463eb5b85df2
rostun <rosa.m.tung@gmail.com>
2016-03-19 17:00:36 -0400

    BFS
    Breadth First Search

---
+++ BFS/BFS/BFS.cpp
@@ -0,0 +1,28 @@
+// BFS.cpp : Defines the entry point for the console application.
+/*
+	Given a tree (generic, not binary, no cycles):
+		A
+	   /  \
+	  B    C
+	/ | \  |\
+	D E  F G H
+		   |
+		   I
+
+	Print the nodes in the following manner (each depth on its own line, followed by a line break):
+	A
+	BC
+	DEFGH
+	I
+*/
+
+#include "stdafx.h"
+#include <iostream> 
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello world" << endl;
+	return 0;
+}
+
