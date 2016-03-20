7c944117e9c740238462ecb3f699e1f8978526f8
rostun <rosa.m.tung@gmail.com>
2016-03-19 20:07:46 -0400

    blah
    asdf

--- BFS/BFS/BFS.cpp
+++ BFS/BFS/BFS.cpp
@@ -18,11 +18,56 @@
 
 #include "stdafx.h"
 #include <iostream> 
+#include <vector>
+#include <string>
+#include <queue>
 using namespace std;
+
+class treeNode{
+	private:
+		string parent; //the parent
+		vector <treeNode*> children; //the parent's children
+	public:
+		treeNode();
+		treeNode(string inParent){ 
+			setParentNode(inParent);
+		}
+		void setParentNode(string inParent){ 
+			parent = inParent;
+		}
+		string getValue(){ 
+			return parent;
+		}
+		vector <treeNode*> getChildren() { 
+			return children;
+		}
+		void addChild(treeNode &inChild){ 
+			children.push_back(&inChild);
+		}
+};
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "hello world" << endl;
+	treeNode root("A");
+	treeNode nodeB("B");
+	treeNode nodeC("C"); root.addChild(nodeB); root.addChild(nodeC);
+	treeNode nodeD("D");
+	treeNode nodeE("E");
+	treeNode nodeF("F"); nodeB.addChild(nodeD); nodeB.addChild(nodeE); nodeB.addChild(nodeF);
+	treeNode nodeG("G");
+	treeNode nodeH("H"); nodeC.addChild(nodeG); nodeC.addChild(nodeH);
+	treeNode nodeI("I"); nodeG.addChild(nodeI);
+
+	//BFS: print out level by level
+	queue <treeNode> nodesToVisit; //queues for nodes
+	nodesToVisit.push(root); 
+	treeNode currentNode = nodesToVisit.front(); //save front of queue
+	root.getChildren()[0];
+	nodesToVisit.pop(); //pop off front of queue
+
+	for(int i = 0; i<currentNode.getChildren().size(); i++){
+		nodesToVisit.push(currentNode.getChildren());
+	}
+
 	return 0;
 }
-
