7e7e933e418ec90402c9dbb3864fa636400c12de
rostun <rosa.m.tung@gmail.com>
2016-04-03 13:24:41 -0400

    guide for containers
    bladsjfsdjf;

---
+++ guides/stlContainersCheatsheet.h
@@ -0,0 +1,8 @@
+unordered_map<type1, type2>
+vector <type> //dynamic array
+map<type1, type2>
+queue<type> //filo
+stack<type> //fifo
+list<type> 
+unordered_set<type> (constant insert/lookup)  
+priority_queue<type> (logn insert)
