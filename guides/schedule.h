6fd81e89478ce38bf4ff2d13ace554f44216ac62
rostun <rosa.m.tung@gmail.com>
2016-04-05 21:38:36 -0400

    adsf
    asdf

---
+++ guides/schedule.h
@@ -0,0 +1,39 @@
+//week 1: get schedule together, discuss goals 
+//week 2: define what containers do, their place in data structures
+//week 3: simple implementations - use every single function in the library
+//week 4: running times of functions, how to iterate through containers
+
+//stl container tools
+unordered_map<type1, type2>
+vector <type> 
+map<type1, type2>
+queue<type> //fifo
+stack<type> //filo
+list<type> 
+unordered_set<type> //constant insert/lookup  
+priority_queue<type> //logn insert
+set<type> //binary search trees
+
+//	**start making list of companies, make sure everyone is on the same sites, apply to companies**
+
+//week 5: start concepts-> go through list of concepts maybe add a few 
+//week 6: define how to implement concepts with containers
+//week 7: go through interview questions, add to algorithm list, write down everyone's weak points
+//week 8: go through interview questions, add to algorithm list, write down everyone's weak points
+//week 9: go through interview questions, add to algorithm list, write down everyone's weak points
+//week 10: go through interview questions, add to algorithm list, write down everyone's weak points
+//week 11+: repeat until we got opportunities
+
+//concepts
+Big O Running times 
+Trees/Graphs
+Breadth First Search
+Depth First Search
+Hash tables 
+Linked Lists
+stacks/queues 
+heaps
+vectors
+arrayLists
+sorts less important 
+object oriented least important (inheritance important)
